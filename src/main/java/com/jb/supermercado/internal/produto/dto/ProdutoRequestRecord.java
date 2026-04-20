c912c000a0ae1c1a2a4fe9bba8d3605371c6ac87
danielacvmelo <danielacvmelo@gmail.com>
2026-04-19 23:11:52 -0300

    feat: adicionando classe produto

--- src/main/java/com/jb/supermercado/internal/produto/dto/ProdutoRequestRecord.java
+++ src/main/java/com/jb/supermercado/internal/produto/dto/ProdutoRequestRecord.java
@@ -1,13 +1,12 @@
 package com.jb.supermercado.internal.produto.dto;
-}
-        String status) {
-        Integer quantidadeEstoque,
-        Double preco,
-        String descricao,
-        String nome,
-        @NotBlank(message = "O campo nome e obrigatorio")
-public record ProdutoRequestRecord(
 
 import jakarta.validation.constraints.NotBlank;
 
-
+public record ProdutoRequestRecord(
+        @NotBlank(message = "O campo nome e obrigatorio")
+        String nome,
+        String descricao,
+        Double preco,
+        Integer quantidadeEstoque,
+        String status) {
+}
